429b199ea12048b91fb3055cffa14ba470525c5d
Patricio Rodriguez <patriciorodriguez@Mac-Pato.local>
2025-02-12 19:13:29 -0300

    Mejora estetica en cards productos y detail

--- src/Components/ProductListContainer/ProductCard.tsx
+++ src/Components/ProductListContainer/ProductCard.tsx
@@ -8,30 +8,32 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
     return (
-        <div className="bg-gray-700 rounded-lg overflow-hidden flex flex-col lg:flex-row">
-            <div className="w-full h-48 lg:w-1/3">
-                <img
-                    src={product.thumbnail}
-                    className="object-cover h-[150px] w-[150px] rounded-t-lg lg:rounded-l-lg lg:rounded-t-none"
-                    alt={product.title}
-                />
-            </div>
+        <div className="bg-gray-700 rounded-lg p-4 mt-4">
+            <div className="flex flex-col items-center lg:flex-row">
+                <div className="flex justify-center p-4 lg:w-1/3">
+                    <img
+                        src={product.thumbnail}
+                        className="object-cover w-40 h-40 rounded-lg"
+                        alt={product.title}
+                    />
+                </div>
 
-            <div className="flex flex-col justify-between p-4 lg:w-2/3 lg:py-4 lg:pl-0">
-                <Link to={`/${product.id}`} style={{ textDecoration: "none" }}>
-                    <h5 className="text-xl font-bold text-white">{product.title}</h5>
-                    <p className="font-normal text-gray-300">
-                        {new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS" }).format(product.price)}
-                    </p>
-                </Link>
-                <a
-                    href={product.permalink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-center text-white bg-blue-700 hover:bg-blue-800 rounded-lg p-2.5 mt-4"
-                >
-                    Ver Producto ML
-                </a>
+                <div className="p-4 text-center lg:text-left lg:w-2/3 lg:py-4 lg:pl-0">
+                    <Link to={`/${product.id}`} style={{ textDecoration: "none" }}>
+                        <h5 className="text-xl font-bold text-white">{product.title}</h5>
+                        <p className="font-normal text-gray-300">
+                            {new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS" }).format(product.price)}
+                        </p>
+                    </Link>
+                    <a
+                        href={product.permalink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block bg-blue-700 hover:bg-blue-800 text-white rounded-lg px-4 py-2 mt-4"
+                    >
+                        Ver Producto ML
+                    </a>
+                </div>
             </div>
         </div>
     )
--- src/Components/ProductListContainer/ProductDetail.tsx
+++ src/Components/ProductListContainer/ProductDetail.tsx
@@ -4,31 +4,28 @@ import { useFetchProductDetail } from "../../Hooks/useFetchProductDetail"
 
 const ProductDetail = () => {
     const { productId } = useParams<{ productId: string }>()
-    const { product, fetchProductDetail, loading, error } = useFetchProductDetail()
+    const { product, fetchProductDetail } = useFetchProductDetail()
 
     useEffect(() => {
         fetchProductDetail(productId!) // Con ! le digo que nunca sera undefined, simepre me va a llegar un id //
     }, [productId])
 
     return (
-        <div>
-            <div className="card mb-3" style={{ width: "540px" }}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src={product?.thumbnail} className="img-fluid rounded-start" alt="..." />
-                        {/* va ? porque me aseguro de no intentar acceder a una propiedad nula */}
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title">{product?.title}</h5>
-                            <p className="card-text">{product?.price}</p>
-                            <p className="card-text"><small className="text-body-secondary">{product?.description}</small></p>
-                            <p className="card-text"><small className="text-body-secondary">{product?.avaliableQuantity}</small></p>
-                        </div>
+        <div className="bg-gray-700 rounded-lg mt-4">
+            <div className="flex flex-col lg:flex-row">
+                <div className="flex justify-center p-4 rounded-lg lg:p-0">
+                    <img src={product?.thumbnail} className="w-full sm:w-80 md:w-96 h-80 rounded-lg object-cover" alt={product?.title} />                    {/* va ? porque me aseguro de no intentar acceder a una propiedad nula */}
+                </div>
+                <div className="p-4">
+                    <div>
+                        <h5 className="text-white text-xl font-bold">{product?.title}</h5>
+                        <p className="font-normal text-gray-300">${product?.price}</p>
+                        <p><small>{product?.description}</small></p>
+                        <p><small>{product?.avaliableQuantity}</small></p>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 
