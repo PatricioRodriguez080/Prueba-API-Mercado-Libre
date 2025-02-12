ea03424163e3ffc0693c5a9d648400c8ab421488
Patricio Rodriguez <patriciorodriguez@Mac-Pato.local>
2025-02-12 14:14:55 -0300

    Mejora estetica en cards categorias

--- src/Components/CategoryListContainer/CategoryList.tsx
+++ src/Components/CategoryListContainer/CategoryList.tsx
@@ -7,7 +7,7 @@ interface CategoryListProps {
 
 const CategoryList = ({ categorias }: CategoryListProps) => {
     return (
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4">
             {categorias.map((categoria) => (
                 <CategoryCard categoria={categoria} key={categoria.id} />
             ))}
