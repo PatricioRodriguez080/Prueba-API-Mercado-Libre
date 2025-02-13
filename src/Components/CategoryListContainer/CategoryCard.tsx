5971759dfd5422269268be022a6905926a0e158a
Patricio Rodriguez <patriciorodriguez@Mac-Pato.local>
2025-02-13 10:16:19 -0300

    Arreglo de imagenes en cards categorias

--- src/Components/CategoryListContainer/CategoryCard.tsx
+++ src/Components/CategoryListContainer/CategoryCard.tsx
@@ -8,9 +8,15 @@ interface CategoryCardProps {
 const CategoryCard = ({ categoria }: CategoryCardProps) => {
     return (
         <Link to={`category/${categoria.id}`}>
-            <div className="bg-gray-700 rounded-lg flex flex-col justify-center">
-                <img src={`/Images/${categoria.name}.webp`} className="h-80 w-full object-cover rounded-md" alt={categoria.name} />
-                <h1 className="text-white text-xl text-center">{categoria.name}</h1>
+            <div className="bg-gray-700 rounded-lg flex flex-col items-center justify-center p-4">
+                <div className="flex items-center justify-center h-80 w-80">
+                    <img
+                        src={`/Images/${categoria.name}.webp`}
+                        className="h-full w-full object-cover rounded-md"
+                        alt={categoria.name}
+                    />
+                </div>
+                <h1 className="text-white text-xl text-center mt-2">{categoria.name}</h1>
             </div>
         </Link>
     )
